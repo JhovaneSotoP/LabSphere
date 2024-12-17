ea3a450eb17e37903c6034a3c6947e2ad51b7a7a
Jhovane <jhovanesoto@gmail.com>
2024-12-16 20:54:45 -0600

    Se mejoro el diseño del registro

--- LabSphere v1.0.1.py
+++ LabSphere v1.0.1.py
@@ -19,11 +19,61 @@ from rich.align import Align
 from rich.bar import Bar
 from rich.text import Text
 from rich.rule import Rule
+import time
 
 usuario="XXXXXX"
 
-version="v1.2.0"
+version="v1.3.0"
 console=Console()
+
+
+#FUNCIONES DE VISTA
+
+def imprimirInicio():
+  titulo = Text("◆ LabSphere " + version + " ◆", style="bold cyan", justify="center")
+
+  desarrollador=Text("● Support: ", style="green",justify="left")
+  desarrollador.append("jhovane.soto@fii-na.com", style="white")
+
+  manual=Text("Click ",style="white", justify="left")
+  manual.append("here", style="magenta link https://chatgpt.com/")
+  manual.append(" to open user manual", style="white")
+
+  usuario_cad = Text("\n ‣ User: " + usuario, style="white", justify="left")
+
+  layout = Layout()
+
+  layout.split(
+      Layout(name="titulo", ratio=1),
+      Layout(name="desarollador", ratio=1),
+      Layout(name="manual", ratio=1),
+      Layout(name="usuario_cad", ratio=1),
+  )
+
+  layout["titulo"].update(titulo)
+  layout["manual"].update(manual)
+  layout["desarollador"].update(desarrollador)
+  layout["usuario_cad"].update(usuario_cad)
+
+  # Crear el panel con el layout ya creado
+  panel = Panel(layout, height=10)
+  console.print(panel)
+
+def imprimirTitulo(text,color):
+  os.system("CLS")
+  imprimir=Text(text,style=f"Bold {color}",justify="center")
+  panel=Panel(imprimir,height=3)
+  console.print(panel)
+
+def imprimirError(text,tiempo=2):
+  console.print(f"[bold red]❌ {text}[/bold red]")
+  time.sleep(tiempo)
+
+def imprimirExito(text,tiempo=2):
+  console.print(f"[bold green]✅ {text}[/bold green]")
+  time.sleep(tiempo)
+
+
 
 #Cargar flujos del sistema desde el JSON
 with open("User Data/data.json", "r") as file:
@@ -35,7 +85,7 @@ def tiempoActual():
 def bfs_shortest_path(flow_type, start, end):
     with open("User Data/data.json", "r") as file:
         graph = json.load(file)
-    
+
     graph=graph[flow_type]
     # Cola para nodos pendientes de visitar: (nodo_actual, distancia)
     queue = deque([(start, 0)])
@@ -96,7 +146,8 @@ class dataBase():
       FLOW TEXT,
       FLOW_STATUS TEXT,
       DATE TEXT,
-      USER TEXT
+      USER TEXT,
+      COMMENTS TEXT
     )""")
 
     self.conn.commit()
@@ -142,7 +193,7 @@ class dataBase():
   def ultimoRL(self):
     self.conn = sqlite3.connect('User Data/data.db')
     cursor = self.conn.cursor()
-    cursor.execute("SELECT CASE_NAME FROM CASES WHERE CASE_NAME LIKE 'RL%' ORDER BY CASE_NAME DESC LIMIT 1")
+    cursor.execute("SELECT CASE_NAME FROM CASES WHERE CASE_NAME LIKE 'RMP%' ORDER BY CASE_NAME DESC LIMIT 1")
     result = cursor.fetchone()
     self.conn.close()
     if result:
@@ -186,7 +237,6 @@ class dataBase():
     cursor = self.conn.cursor()
     cursor.execute("SELECT NEXT_FLOW FROM SAMPLES WHERE SERIAL = ? AND COMPONENT = ?", (serial, sample))
     result = cursor.fetchone()
-    print(result)
     self.conn.close()
     if result:
       return result[0]
@@ -264,7 +314,7 @@ class dataBase():
       return result[0]
     else:
       return None
-  
+
   def retornarFlujos(self,id):
     self.conn = sqlite3.connect('User Data/data.db')
     cursor = self.conn.cursor()
@@ -275,7 +325,7 @@ class dataBase():
     if result:
       for n in result:
         salida.append(n[0])
-    
+
     vistos=set()
     resultado=[]
 
@@ -285,32 +335,32 @@ class dataBase():
           resultado.append(n)
 
     return resultado
-  
+
   def actualizarPorcentajeMuestra(self,serial,sample,percentaje):
     self.conn = sqlite3.connect('User Data/data.db')
     cursor = self.conn.cursor()
     cursor.execute("UPDATE SAMPLES SET PERCENTAGE = ? WHERE SERIAL = ? AND COMPONENT = ?", (percentaje, serial, sample))
     self.conn.commit()
     self.conn.close()
-  
+
   def actualizarPorcentajeCaso(self,serial,percentaje):
     self.conn = sqlite3.connect('User Data/data.db')
     cursor = self.conn.cursor()
     cursor.execute("UPDATE CASES SET PERCENTAGE = ? WHERE SERIAL = ?", (percentaje, serial))
     self.conn.commit()
     self.conn.close()
-  
+
   def muestrasPorcentaje(self,serial):
     self.conn = sqlite3.connect('User Data/data.db')
     cursor = self.conn.cursor()
     cursor.execute("SELECT PERCENTAGE FROM SAMPLES WHERE SERIAL = ?", (serial,))
     result = cursor.fetchall()
     self.conn.close()
-    
+
     salida=[]
     for n in result:
        salida.append(result[0])
-    
+
     return salida
 
   def extraerFlujoMenor(self,serial):
@@ -320,14 +370,14 @@ class dataBase():
     result = cursor.fetchall()
     self.conn.close()
     return result
-  
+
   def actualizarFlujoCaso(self,serial,flujo):
     self.conn = sqlite3.connect('User Data/data.db')
     cursor = self.conn.cursor()
     cursor.execute("UPDATE CASES SET STATUS = ? WHERE SERIAL = ?", (flujo, serial))
     self.conn.commit()
     self.conn.close()
-  
+
   def cerrar(self):
     self.conn.close()
 
@@ -353,14 +403,15 @@ class conexionLab(dataBase):
           case_name=self.dataBase.ultimoRL()
           if case_name:
               case_number=int(case_name[2:])+1
-              case_name = "RL" + f"{case_number:03}"
+              case_name = "RMP" + f"{case_number:03}"
           else:
-              case_name="RL001"
+              case_name="RMP001"
         else:
           case_name=MP
 
         fechaActual=tiempoActual()
         self.dataBase.registrarCaso(serial,case_name,modelo,tipoProceso,len(componentes),requisitor,MP,tiempo,"","REGISTER",0.0,"")
+        imprimirExito(f"Case {case_name} registered", tiempo=0)
 
         #Registar samples and samples changes
         #SERIAL, COMPONENT ,DATE_IN , DATE_OUT , FLOW_CUR ,NEXT_FLOW ,PERCENTAGE ,ON_HOLD ,PRIORITY ,COMMENTS
@@ -380,7 +431,8 @@ class conexionLab(dataBase):
           #agregar movimiento in y out en los cambios
           self.dataBase.registrarCambio(id,"REGISTER","IN",tiempo,usuario)
           self.dataBase.registrarCambio(id,"REGISTER","OUT",fechaActual,usuario)
-
+          imprimirExito(f"Sample {component} registered", tiempo=0)
+        time.sleep(2)
 
     def serialAndSampleIntoDB(self,serial,sample):
         data=self.dataBase.consultarTodo("SAMPLES","SERIAL",serial)
@@ -394,7 +446,7 @@ class conexionLab(dataBase):
         if data:
           return data.split(", ")
         else:
-          print("The sample´s flow is already finished")
+          imprimirError("The sample´s flow is already finished")
           return []
     def sampleCurrentFlow(self,serial,sample):
         return self.dataBase.flujoActual(serial,sample)
@@ -439,7 +491,7 @@ class conexionLab(dataBase):
         self.dataBase.registrarCambio(id,newFlow,"IN",actual,usuario)
 
         self.actualizarPorcentajes(serial,sample)
-    
+
     def actualizarPorcentajes(self, serial,sample):
       id=self.dataBase.regresarID(serial,sample)
       flujosMuestra=self.dataBase.retornarFlujos(id)
@@ -456,7 +508,6 @@ class conexionLab(dataBase):
       promedio=total/len(data)
 
       self.dataBase.actualizarPorcentajeCaso(serial,promedio)
-      print(self.dataBase.extraerFlujoMenor(serial))
       self.dataBase.actualizarFlujoCaso(serial,self.dataBase.extraerFlujoMenor(serial)[0][0])
 
 
@@ -471,9 +522,10 @@ labDB=conexionLab()
 
 #MODO LABORATORIO
 def laboratorio(proceso):
-    imprimirTitulo("Laboratory","magenta")
+
     flujo=proceso
     if flujo=="REGISTER":
+        imprimirTitulo("Laboratory","magenta")
         console.print(f"[magenta]Complete the following fields to [bold]REGISTER[/bold] a case[/magenta]")
         serial=input("Enter serial number: ").upper()
 
@@ -484,7 +536,7 @@ def laboratorio(proceso):
 
         #Comprobar si el serial esta en la base de datos
         if(labDB.serialIntoDB(serial)):
-            print("Serial already in use")
+            imprimirError("Serial already in use")
         else:
             modelo=input("Enter model: ").upper()
 
@@ -526,7 +578,6 @@ def laboratorio(proceso):
 
                     #salir al inicio si se presiona E
                     if(component=="E"):
-                        print("Saliendo")
                         break
 
                     componentes.append(component)
@@ -534,11 +585,11 @@ def laboratorio(proceso):
                 if(len(componentes)>0):
                     labDB.registrarCaso(serial,modelo,tipoProceso,requisitor,MP,tiempo,componentes)
                 else:
-                    print("Invalid quantity of components")
+                    imprimirError("Invalid quantity of components")
                     return
 
             else:
-                print("Invalid type of process")
+                imprimirError("Invalid type of process")
                 return
     else:
         #extraer todos los flujos existentes
@@ -549,6 +600,8 @@ def laboratorio(proceso):
 
         #Comprobar si el flujo ingresado existe
         if(flujo in flujos):
+            imprimirTitulo("Laboratory","magenta")
+            console.print(f"[magenta] ➤ [bold]{flujo}[/bold][/magenta]")
             #Escanear el codigo de barras de la muestra
             muestra=input("Scan sample´s barcode:").upper()
 
@@ -559,13 +612,23 @@ def laboratorio(proceso):
                 print("Saliendo")
                 return
 
-            if len(muestra)!=2:
-                print("Invalid barcode")
+            if(len(muestra)==1):
+                sample=input("Enter sample: ").upper()
+                if(sample=="E"):
+                    print("Saliendo")
+                    return
+                muestra.append(sample)
+            elif len(muestra)!=2:
+                imprimirError("Invalid barcode")
                 return
 
             #comprobar si el serial y muestra estan en la base de datos
             serial=muestra[0]
             sample=muestra[1]
+
+            #Si sample es ALL extraer los nombres de todas las muestras asociadas al serial y tratar de darles el pase
+
+
             if(labDB.serialAndSampleIntoDB(serial,sample)):
                 #Extraer el actual flujo de la muestra y serial
                 currentFlow=labDB.sampleCurrentFlow(serial,sample)
@@ -582,7 +645,7 @@ def laboratorio(proceso):
                           labDB.changeFlowStatus(serial,sample)
                         labDB.toEndSample(serial,sample)
                     else:
-                        print("Serial and sample can´t to END")
+                        imprimirError("Serial and sample can´t to END")
                         return
                 else:
 
@@ -591,56 +654,45 @@ def laboratorio(proceso):
                     if(flujo==currentFlow):
                         if flowStatus=="IN":
                             labDB.changeFlowStatus(serial,sample)
+                            imprimirExito(f"Sample {sample} moved to {flujo} OUT")
                         else:
-                            print("Error, this flow is already give it")
+                            imprimirError("Error, this flow is already give it")
                             return
                     #En caso que no y que la muestra tenga flujo out hacer lo siguiente
                     elif(flowStatus=="OUT"):
                         #Si esta el flujo en los siguientes flujos, hacer el cambio
                         if(flujo in nextFlow):
                             labDB.changeFlow(serial,sample,flujo)
+                            imprimirExito(f"Sample {sample} moved to {flujo}")
                         else:
-                            print("Error, flow is incorrect")
+                            imprimirError("Error, flow is incorrect")
                             return
                     elif(flowStatus=="IN"):
                         #Si esta el flujo en los siguientes flujos, hacer el cambio aunque no este salida del flujo y hacer el cambio de flujo
                         if(flujo in nextFlow):
                             labDB.changeFlowStatus(serial,sample)
                             labDB.changeFlow(serial,sample,flujo)
+                            imprimirExito(f"Sample {sample} moved to {flujo}")
                         else:
-                            print("Error, flow is incorrect")
+                            imprimirError("Error, flow is incorrect")
                             return
                     elif(flowStatus=="N/A"):
-                        print("Error, flow is incorrect")
+                        imprimirError("Error, flow is incorrect")
                         return
                     else:
-                        print("Error, curretn flow needs to OUT")
+                        imprimirError("Error, current flow needs to OUT")
                         return
 
 
             else:
-                print("Serial and sample is not into DB")
+                imprimirError("Serial and sample is not into DB")
                 return
 
         else:
-            print("Invalid flow")
+            imprimirError("Invalid flow")
             return
 
-def imprimirInicio():
-  os.system("CLS")
-  ascii_art = pyfiglet.figlet_format("LabSphere "+version, font="ogre")
-  #console.print(f"[blue]{ascii_art}[/blue]")
-  console.print(Panel(f"[blue]{ascii_art}[/blue]", border_style="cyan"))
-  console.print(f"[cyan]Welcome {usuario}![/cyan]")
-  console.print(Rule("",style="cyan"))
 
-def imprimirTitulo(text,color):
-  os.system("CLS")
-  ascii_art = pyfiglet.figlet_format(text, font="standard")
-  console.print(Panel(f"[{color}]{ascii_art}[/{color}]", border_style=color))
-  console.print(Rule("",style=color))
-  #[]
-  #{}
 
 
 while(1):
@@ -663,7 +715,7 @@ while(1):
     if(data[0]=="E"):
         print("Saliendo")
         break
-    
+
     imprimirInicio()
     data=input("Scan a QR to continue: ").upper()
     #Procesar QR
@@ -681,13 +733,12 @@ while(1):
       proceso=""
       labDB.dataBase.reporte()
     else:
-      print("QR invalido")
+      imprimirError("Invalid QR")
       continue
 
-    
+
 
     if tipo=="LAB":
       laboratorio(proceso)
     elif(tipo=="USR"):
       usuario=proceso
-      
